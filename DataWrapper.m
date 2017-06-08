9064941485b3636b9117cc055fb9de02c9173f9f
Philipp Meschenmoser <Philipp.Meschenmoser@uni.kn>
2017-06-08 18:30:51 +0200

    python communication

--- DataWrapper.m
+++ DataWrapper.m
@@ -3,18 +3,27 @@ classdef DataWrapper
     end
     
     methods
-        function obj = DataWrapper(path, max)
-            if nargin > 0
-                py.preprocessor.run(path, max)
+        function obj = DataWrapper()
+            % Constructor
+            % Add the current folder to the python search path
+            if count(py.sys.path,'') == 0
+                insert(py.sys.path,int32(0),'');
             end
         end
     end
     
     methods (Static)
-        function r = importFromFolder(path,max)
-            r = py.preprocessor.run(path, max); 
+        function r = importFromFolder(path)
+            dirlist = dir(strcat(path,'*.txt'));
+            a = 0; 
+            c = {}; 
+            for i = 1:length(dirlist)
+                c{1,i} = strcat(path,dirlist(i).name);
+            end
+            % we need a cell area over here
+            l = py.list(c); 
+            r = py.preprocessor.run(l); 
         end   
     end
-    
 end
 
--- preprocessor.py
+++ preprocessor.py
@@ -4,28 +4,18 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import re
 from collections import Counter
-import glob, os
 
-def run(directory, maxcount):
+
+def run(paths):
     lemmatizer = WordNetLemmatizer()
     stops = set(stopwords.words('english'))
-    os.chdir(directory)
     result = [];
-    count = 0;
-    for file in glob.glob('*.txt'):
-        if maxcount is not None and maxcount < count:
-            return result;
-        with open(file, 'r+') as f:
+    for file in paths:
+        with open(file, 'r') as f:
             sentences = sent_tokenize(f.read().lower())  # lower casing and sentence tokenization
             tokens = []
             for s in sentences:
                 tokens = tokens + [re.sub('[^A-Za-z0-9]+', '', lemmatizer.lemmatize(t)) for t in word_tokenize(s) if t not in stops]
                 tokens = [t for t in tokens if len(t) > 0] #  to-do: optimize regex above, so that we dont need this iteration
-            result.append(Counter(tokens))
-            count+=1;
+            result.append(dict(Counter(tokens)))
     return result
-
-if __name__ == "__main__":
-    #  change the path to your file!
-    preprocessed = run("D:/enron/pre2006/enron1/ham/",100)
-    print(preprocessed)
